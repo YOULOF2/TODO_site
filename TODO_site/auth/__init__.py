7456eacef4283a96645edcc517967446352a8924
YOULOF2 <youlof123@gmail.com>
2021-10-25 19:59:49 +0200

    Create the authentication and login or sign up of users

--- TODO_site/auth/__init__.py
+++ TODO_site/auth/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash, current_app
+from flask import Blueprint, render_template, request, redirect, url_for, flash, abort
 from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
 from .forms import SignUp, Login, ActionType
@@ -28,70 +28,77 @@ def home():
 
 @auth.route("/user/welcome", methods=["GET", "POST"])
 def welcome():
-    print("WELCOME")
-    action_form = ActionType()
-    if request.method == "POST":
-        if action_form.validate_on_submit():
-            if action_form.login.data:
-                # return "<h1>LOGIN</h1>"
-                return redirect(url_for("auth.user_login"))
-            else:
-                # return "<h1>Signup</h1>"
-                return redirect(url_for("auth.user_signup"))
+    if current_user.is_anonymous:
+        print("WELCOME")
+        action_form = ActionType()
+        if request.method == "POST":
+            if action_form.validate_on_submit():
+                if action_form.login.data:
+                    # return "<h1>LOGIN</h1>"
+                    return redirect(url_for("auth.user_login"))
+                else:
+                    # return "<h1>Signup</h1>"
+                    return redirect(url_for("auth.user_signup"))
 
-    return render_template("home.html", form=action_form)
+        return render_template("home.html", form=action_form)
+    return abort(403)
 
 
 @auth.route("/user/login", methods=["GET", "POST"])
 def user_login():
-    login_form = Login()
-    # POST METHOD
-    if request.method == "POST":
-        if login_form.validate_on_submit():
-            email = login_form.email.data
-            password = login_form.password.data
-            user = User.query.filter_by(email=email).first()
-            if user is not None:
-                if check_password_hash(password, user.password):
-                    login_user(user)
-                    return "<h1>HELLO LOGIN</h1>"
+    if current_user.is_anonymous:
+        login_form = Login()
+        # POST METHOD
+        if request.method == "POST":
+            if login_form.validate_on_submit():
+                email = login_form.email.data
+                password = login_form.password.data
+                user = User.query.filter_by(email=email).first()
+                if user is not None:
+                    if check_password_hash(password, user.password):
+                        login_user(user)
+                        return "<h1>HELLO LOGIN</h1>"
+                    else:
+                        flash("Wrong password, please redo")
+                        return redirect(url_for("auth.user_login"))
                 else:
-                    flash("Wrong password, please redo")
-                    return redirect(url_for("auth.user_login"))
-            else:
-                flash("You don't have an account. Sign up to continue.")
-                return redirect(url_for("auth.user_signup"))
-    return render_template("login.html", form=login_form)
+                    flash("You don't have an account. Sign up to continue.")
+                    return redirect(url_for("auth.user_signup"))
+        return render_template("login.html", form=login_form)
+    else:
+        return abort(403)
 
 
 @auth.route("/user/signup", methods=["GET", "POST"])
 def user_signup():
-    signup_form = SignUp()
-    # POST METHOD
-    if request.method == "POST":
-        if signup_form.validate_on_submit():
-            name = signup_form.name.data
-            email = signup_form.email.data
-            password = signup_form.password.data
-            re_password = signup_form.re_password.data
-            # with current_app.app_context():
-            if User.query.filter_by(email=email).first() is None:
-                if password == re_password:
-                    pass_hash = generate_password_hash(password, salt_length=SALT_TIMES)
-                    user = User(
-                        name=name,
-                        email=email,
-                        password=pass_hash,
-                    )
-                    db.session.add(user)
-                    db.session.commit()
-                    login_user(user)
-                    return f"<h1>HELLO {name}</h1>"
+    if current_user.is_anonymous:
+        signup_form = SignUp()
+        # POST METHOD
+        if request.method == "POST":
+            if signup_form.validate_on_submit():
+                name = signup_form.name.data
+                email = signup_form.email.data
+                password = signup_form.password.data
+                re_password = signup_form.re_password.data
+                # with current_app.app_context():
+                if User.query.filter_by(email=email).first() is None:
+                    if password == re_password:
+                        pass_hash = generate_password_hash(password, salt_length=SALT_TIMES)
+                        user = User(
+                            name=name,
+                            email=email,
+                            password=pass_hash,
+                        )
+                        db.session.add(user)
+                        db.session.commit()
+                        login_user(user)
+                        return f"<h1>HELLO {name}</h1>"
+                    else:
+                        flash("You retyped the password incorrectly, try again.")
+                        return redirect(url_for("auth.user_signup"))
                 else:
-                    flash("You retyped the password incorrectly, try again.")
-                    return redirect(url_for("auth.user_signup"))
-            else:
-                flash("You are already registered. Please login.")
-                return redirect(url_for("auth.user_login"))
-
-    return render_template("sign_up.html", form=signup_form)
+                    flash("You are already registered. Please login.")
+                    return redirect(url_for("auth.user_login"))
+        return render_template("sign_up.html", form=signup_form)
+    else:
+        return abort(403)
