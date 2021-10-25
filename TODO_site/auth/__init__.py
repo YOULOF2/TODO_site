46add2eee440b4a8fd9236e752f05537400f9068
YOULOF2 <youlof123@gmail.com>
2021-10-25 19:43:28 +0200

    Create the authentication and login or sign up of users

--- TODO_site/auth/__init__.py
+++ TODO_site/auth/__init__.py
@@ -1,10 +1,12 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash, current_app
 from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
 from .forms import SignUp, Login, ActionType
-from TODO_site.database import db as database
+from TODO_site.database import db
 from TODO_site.database import User
-from flask_login import LoginManager, current_user
+from flask_login import LoginManager, current_user, login_user
+
+SALT_TIMES = 50
 
 auth = Blueprint('auth', __name__, template_folder='templates')
 
@@ -13,10 +15,10 @@ login_manager = LoginManager()
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.get(user_id)
+    return User.query.get(user_id)
 
 
-@auth.route("/", methods=["GET", "POST"])
+@auth.route("/", methods=["GET"])
 def home():
     if current_user.is_authenticated:
         return "<h1>YOU ARE AUTHENTICATED</h1>"
@@ -24,14 +26,17 @@ def home():
         return redirect(url_for("auth.welcome"))
 
 
-@auth.route("/user/welcome")
+@auth.route("/user/welcome", methods=["GET", "POST"])
 def welcome():
+    print("WELCOME")
     action_form = ActionType()
     if request.method == "POST":
         if action_form.validate_on_submit():
             if action_form.login.data:
+                # return "<h1>LOGIN</h1>"
                 return redirect(url_for("auth.user_login"))
             else:
+                # return "<h1>Signup</h1>"
                 return redirect(url_for("auth.user_signup"))
 
     return render_template("home.html", form=action_form)
@@ -47,7 +52,15 @@ def user_login():
             password = login_form.password.data
             user = User.query.filter_by(email=email).first()
             if user is not None:
-                pass
+                if check_password_hash(password, user.password):
+                    login_user(user)
+                    return "<h1>HELLO LOGIN</h1>"
+                else:
+                    flash("Wrong password, please redo")
+                    return redirect(url_for("auth.user_login"))
+            else:
+                flash("You don't have an account. Sign up to continue.")
+                return redirect(url_for("auth.user_signup"))
     return render_template("login.html", form=login_form)
 
 
@@ -61,4 +74,24 @@ def user_signup():
             email = signup_form.email.data
             password = signup_form.password.data
             re_password = signup_form.re_password.data
+            # with current_app.app_context():
+            if User.query.filter_by(email=email).first() is None:
+                if password == re_password:
+                    pass_hash = generate_password_hash(password, salt_length=SALT_TIMES)
+                    user = User(
+                        name=name,
+                        email=email,
+                        password=pass_hash,
+                    )
+                    db.session.add(user)
+                    db.session.commit()
+                    login_user(user)
+                    return f"<h1>HELLO {name}</h1>"
+                else:
+                    flash("You retyped the password incorrectly, try again.")
+                    return redirect(url_for("auth.user_signup"))
+            else:
+                flash("You are already registered. Please login.")
+                return redirect(url_for("auth.user_login"))
+
     return render_template("sign_up.html", form=signup_form)
--- TODO_site/database/__init__.py
+++ TODO_site/database/__init__.py
@@ -11,6 +11,7 @@ class User(db.Model, UserMixin):
     name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.Text, nullable=False)
+
     cards = relationship("Cards", back_populates="user")
 
     def __repr__(self):
@@ -24,7 +25,9 @@ class Cards(db.Model):
     title = db.Column(db.Text, nullable=False)
     sub_title = db.Column(db.Text, nullable=False)
     body = db.Column(db.Text, unique=True, nullable=False)
-    cards = relationship("User", back_populates="cards")
+
+    user = relationship("User", back_populates="cards")
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         print(f"There are {self.title.count()} records in  the table\n"
